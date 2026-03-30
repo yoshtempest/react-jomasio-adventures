ab0fa115e60f8b870bb813e9c74635cb4f06e4f6
yoshtempest <marcelobenjaminrocha100@gmail.com>
2026-03-30 13:59:46 -0300

    new: hallOne and hallTwo

---
+++ src/pages/Hall/First/index.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { usePlayer } from "@/contexts/PlayerContext";
+import styles from "./styles.module.css";
+import { hallOne } from "@/maps/hallOne";
+import { useGameLayout } from "@/hooks/useGameLayout";
+import { GameMap } from "@/components/Game/GameMap";
+import { Player } from "@/components/Game/Player";
+
+export default function HallOne() {
+  const { player, setMap } = usePlayer();
+  const navigate = useNavigate();
+
+  const { TILE_SIZE, offsetX, offsetY, PLAYER_SIZE, MAP_COLS, MAP_ROWS } = useGameLayout();
+
+  useEffect(() => {
+    if (player.gridX === 6 && player.gridY === 7) {
+      navigate("/cantina");
+    }
+  }, [player]);
+
+  useEffect(() => {
+    setMap(hallOne);
+  }, [setMap]);
+
+  return (
+    <div className={`Master ${styles.image}`}>
+      <GameMap
+        TILE_SIZE={TILE_SIZE}
+        offsetX={offsetX}
+        offsetY={offsetY}
+        cols={MAP_COLS}
+        rows={MAP_ROWS}
+      >
+        <Player
+          direction={player.direction}
+          gridX={player.gridX}
+          gridY={player.gridY}
+          TILE_SIZE={TILE_SIZE}
+          PLAYER_SIZE={PLAYER_SIZE}
+        />
+      </GameMap>
+    </div>
+  );
+}
