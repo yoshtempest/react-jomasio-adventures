c0b11dec3e8b4ce45d0c7edd9e4fe3d26f51e25e
marcelo <marcelobenjaminrocha100@gmail.com>
2026-04-07 16:57:46 -0300

    new: interaction in director.ts

--- src/data/maps/director/messages.ts
+++ src/data/maps/director/messages.ts
@@ -1,6 +1,7 @@
 export const directorMessages: Record<string, string> = {
     "6,4": "imagens em preto e branco... deve ter falto tinta...",
     "5,4": "Diário de Reincardion cap 2... Encontrei a chave, mas fui pego por Jhow Simar e jogado aqui novamente...",
+    "13,4": "Uma carta bem velha: Damn Reincarnation vai ter cap novo? Tomará...",
     "14,5": "Uma imagem de Vandinha montada em um dinossauro... Como tiraram essa foto?",
     "15,6": "Uma carta de 20 anos atrás... Pedido de afastamento por Slimita...",
     "7,4": "Chaves? Chaves! Aquele do barril, e pensar que teria uma foto aqui",
