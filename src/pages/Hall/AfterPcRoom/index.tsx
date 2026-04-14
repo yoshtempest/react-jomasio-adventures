6b05d734763efcd7d5e1011a0205fbb8c5e4945d
yoshtempest <marcelobenjaminrocha100@gmail.com>
2026-04-14 09:31:49 -0300

    new: afterPcRoom page

---
+++ src/pages/Hall/AfterPcRoom/index.tsx
@@ -0,0 +1,49 @@
+import styles from "./styles.module.css";
+import { hallOne } from "@/maps/hall/hallOne";
+import { SceneWithDialogue } from "@/components/SceneWithDialogue";
+import { pcsRoomFiveDialogue } from "@/data/maps/pcsRoom/pcsRoomFive";
+import MonkeyCircle from "@/assets/songs/MonkeyCircle.m4a";
+
+export default function AfterPcRoom() {
+  return (
+    <div className={`Master ${styles.image}`}>
+      <SceneWithDialogue
+        map={hallOne}
+        dialogueData={pcsRoomFiveDialogue}
+        audio={{src: MonkeyCircle}}
+        nextRoute={"/pcroom/six"}
+
+        initialPosition={{ x: 12, y: 7, direction: "left" }}
+        // transitions={[
+        //   {
+        //     positions: [
+        //       { x: 8, y: 11 },
+        //       { x: 9, y: 11 },
+        //       { x: 10, y: 11 },
+        //     ],
+        //     to: "/cantina/three",
+        //   },
+        //   {
+        //     positions: [{ x: 13, y: 7 }],
+        //     to: "/pcroom/one",
+        //   },
+        //   {
+        //     positions: [
+        //       { x: 7, y: 2 },
+        //       { x: 8, y: 2 },
+        //       { x: 9, y: 2 },
+        //     ],
+        //     to: "/hall/two",
+        //   },
+        // ]}
+        npcs={[
+          {
+            src: "/src/assets/npcs/remedinha/default.svg",
+            gridX: 11,
+            gridY: 7,
+          },
+        ]}
+      />
+    </div>
+  );
+}
