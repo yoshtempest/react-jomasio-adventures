4ecc3f42db24a9fa2583f7d809671f3bd59e686c
yoshtempest <marcelobenjaminrocha100@gmail.com>
2026-04-26 10:02:52 -0300

    changing names

--- src/data/options/characters.ts
+++ src/data/options/characters.ts
@@ -1,16 +1,16 @@
 import type { CharacterOption } from "@/utils/types/player/character";
 
 export const CHARACTERS: CharacterOption[] = [
-  { name: "Marcelo", image: "marcelo", selectable: true },
+  { name: "Shadow", image: "marcelo", selectable: true },
   { name: "Eduarda", image: "eduarda", selectable: true },
-  { name: "Samuel", image: "samuel", selectable: false },
-  { name: "Artur", image: "artur", selectable: false },
-  { name: "Emanuel", image: "emanuel", selectable: false },
+  { name: "Samurion", image: "samuel", selectable: false },
+  { name: "Sr. Guaxinim", image: "artur", selectable: false },
+  { name: "Ematron", image: "emanuel", selectable: false },
   { name: "Larissa", image: "larissa", selectable: false },
   { name: "Mayra", image: "mayra", selectable: false },
   { name: "Camilly", image: "camilly", selectable: false },
-  { name: "Lucas", image: "lucas", selectable: false },
-  { name: "Lucauã", image: "lucaua", selectable: false },
-  { name: "Riquelme", image: "riquelme", selectable: false },
-  { name: "Hiago", image: "hiago", selectable: false },
+  { name: "Yvel", image: "lucas", selectable: false },
+  { name: "Babidi", image: "lucaua", selectable: false },
+  { name: "Riquelson", image: "riquelme", selectable: false },
+  { name: "Hopade", image: "hiago", selectable: false },
 ];
