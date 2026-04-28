21107085425e8665d76d6d6c5d4c133168de9577
marcelo <marcelobenjaminrocha100@gmail.com>
2026-04-28 12:08:22 -0300

    fix: changing names to correct form

--- src/data/options/characters.ts
+++ src/data/options/characters.ts
@@ -2,12 +2,12 @@ import type { CharacterOption } from "@/utils/types/player/character";
 
 export const CHARACTERS: CharacterOption[] = [
   { name: "Shadow", image: "marcelo", selectable: true },
-  { name: "Eduarda", image: "eduarda", selectable: true },
+  { name: "Drika", image: "eduarda", selectable: true },
   { name: "Samurion", image: "samuel", selectable: false },
   { name: "Sr. Guaxinim", image: "artur", selectable: false },
   { name: "Ematron", image: "emanuel", selectable: false },
-  { name: "Larissa", image: "larissa", selectable: false },
-  { name: "Mayra", image: "mayra", selectable: false },
+  { name: "Laricell", image: "larissa", selectable: false },
+  { name: "YRA", image: "mayra", selectable: false },
   { name: "Camilly", image: "camilly", selectable: false },
   { name: "Yvel", image: "lucas", selectable: false },
   { name: "Babidi", image: "lucaua", selectable: false },
