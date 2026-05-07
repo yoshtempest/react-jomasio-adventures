e0b3f1602e3a24aaaf95cb38d71a2f70883f77e5
marcelo <marcelobenjaminrocha100@gmail.com>
2026-05-07 16:05:03 -0300

    new: victor sprites + config dialogue

---
+++ src/data/maps/configs.ts
@@ -0,0 +1,50 @@
+export const configsDialogue = [
+    {
+        src: "/src/assets/npcs/victor/lyingDown.svg",
+        name: "Victor",
+        message:
+            "...",
+    },
+    {
+        src: "/src/assets/npcs/victor/teleport.svg",
+        name: "Victor",
+        message:
+            "...",
+    },
+    {
+        src: "/src/assets/npcs/victor/talking.svg",
+        name: "Victor",
+        message:
+            "Prazer, meu nome é Victor, eu sou um Novato nesse trampo de 'sistema' e estou aqui para lhe ensinar o básico",
+    },
+    {
+        src: "/src/assets/npcs/victor/pointing.svg",
+        name: "Victor",
+        message:
+            "Bem, tipo assim, você-",
+    },
+    {
+        isPlayer: true,
+        name: "Protagonista",
+        message:
+            "NUNCA MAIS FAÇA ISSO!",
+    },
+    {
+        src: "/src/assets/npcs/victor/sitting.svg",
+        name: "Victor",
+        message:
+            "Me perdoe... estou pegando esse péssimo hábito do Juan Derson.",
+    },
+    {
+        src: "/src/assets/npcs/victor/sitting.svg",
+        name: "Victor",
+        message:
+            "Enfim, o Juan Derson deve ter lhe ensinado alguma coisa no ano passado, então, quero seminário para amanhã, boa sorte",
+    },
+    {
+        src: "/src/assets/npcs/victor/sitting.svg",
+        name: "Victor",
+        message:
+            "Não se preoucupe, que você vai sair daqui um Doutor! Tchau.",
+    },
+];
