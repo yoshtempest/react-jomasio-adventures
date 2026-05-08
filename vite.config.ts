668899db7b028bb9865ea6d7dca6e4261397b5fd
marcelo <marcelobenjaminrocha100@gmail.com>
2026-05-08 08:31:30 -0300

    finally we can install the game

--- vite.config.ts
+++ vite.config.ts
@@ -41,4 +41,16 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (p) => p.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
