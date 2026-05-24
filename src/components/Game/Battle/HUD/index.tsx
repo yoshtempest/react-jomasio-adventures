228f8a2fd648e40f5490281564e70015cc937036
marcelo <marcelobenjaminrocha100@gmail.com>
2026-05-24 12:49:08 -0300

    new: images and names in battleHud but actually its hardcoded

--- src/components/Game/Battle/HUD/index.tsx
+++ src/components/Game/Battle/HUD/index.tsx
@@ -1,5 +1,7 @@
 import { HealthBar } from "@/components/Game/HealthBar";
 import { Deliciometro } from "@/components/Game/Deliciometro";
+import { asset } from "@/utils/asset";
+import styles from "./styles.module.css";
 
 
 type Props = {
@@ -10,19 +12,37 @@ type Props = {
 export function BattleHUD({ battle, npcStats }: Props) {
   return (
     <>
-      <div style={{ position: "absolute", top: 20, left: 20 }}>
-        <HealthBar hp={battle.playerHP} maxHp={battle.playerMaxHp} />
-      </div>
-
-      <div style={{ position: "absolute", top: 42, left: 20 }}>
-        <Deliciometro
-          delicia={battle.delicia}
-          hitsToSpecial={battle.hitsToSpecial}
+      <div className={styles.container} style={{ left: 10, top: 10 }}>
+        <img
+          src={asset(`/assets/player/eduarda/default.svg`)}
+          alt="Battle HUD"
+          className={styles.image}
         />
+
+        <div style={{ position: "absolute", top: 0, left: 80 }}>
+          <h2 className={styles.playerName}>Nome de usuário</h2>
+          <div className={styles.flexRow}>
+            <HealthBar hp={battle.playerHP} maxHp={battle.playerMaxHp} />
+            <Deliciometro
+              delicia={battle.delicia}
+              hitsToSpecial={battle.hitsToSpecial}
+            />
+          </div>
+        </div>
       </div>
 
-      <div style={{ position: "absolute", top: 20, right: 20 }}>
-        <HealthBar hp={battle.npcHP} maxHp={npcStats.hp} />
+      <div className={styles.container} style={{ right: 10, top: 10 }}>
+        <div style={{ position: "absolute", top: 0, right: 80 }}>
+          <h2 className={styles.name}>Npc Type Convertido</h2>
+          <div style={{ transform: "scaleX(-1)" }}>
+            <HealthBar hp={battle.npcHP} maxHp={npcStats.hp} />
+          </div>
+        </div>
+        <img
+          src={asset(`/assets/npcs/slimita/slime.svg`)}
+          alt="Battle HUD"
+          className={styles.image}
+        />
       </div>
     </>
   );
