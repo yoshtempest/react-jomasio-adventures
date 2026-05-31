54dbbb964d772539739b33a81195cc66ce7d954d
marcelo <marcelobenjaminrocha100@gmail.com>
2026-05-31 12:33:33 -0300

    fix: adjusting position of npc in battle

--- src/components/Game/Npc/Battle/index.tsx
+++ src/components/Game/Npc/Battle/index.tsx
@@ -81,7 +81,7 @@ export function NPCBattle({
         height: TILE_SIZE * sizeMultiplier,
         left: x * scaleX,
         top: y * scaleY,
-        transform: `translate(0%, -30%)`,
+        transform: `translate(-50%, -30%)`,
         zIndex: 9,
       }}
     >
