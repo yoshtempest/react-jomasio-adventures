722f12dbfa69f5c5cb0528d18c53a6fcdb0e7724
marcelo <marcelobenjaminrocha100@gmail.com>
2026-06-04 10:31:55 -0300

    fix: removing unused code of CantinaScene.tsx

--- src/features/cantina/index.tsx
+++ src/features/cantina/index.tsx
@@ -1,12 +1,10 @@
 import { useMemo, useState } from "react";
-import { useLocation, useNavigate } from "react-router";
 
 import { SceneBase } from "@/components/Game/Scenes/Base";
 import { CANTINA_SCENES } from "@/scenes/cantina";
 import { createCantina } from "@/interactions/cantina";
 
 import { useInventory } from "@/contexts/InventoryContext";
-import { useQuests } from "@/contexts/QuestContext";
 import type { SceneId } from "@/utils/types/maps/sceneConfig";
 
 import Talking from "@/components/Talking";
@@ -17,11 +15,6 @@ type Props = {
 
 export function CantinaScene({ sceneId }: Props) {
   const scene = CANTINA_SCENES[sceneId];
-
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  const { quests } = useQuests();
   const { addItem } = useInventory();
 
   const [popup, setPopup] = useState<string | null>(null);
@@ -47,42 +40,6 @@ export function CantinaScene({ sceneId }: Props) {
         interactions={interactions}
         popup={popup}
         setPopup={setPopup}
-
-        // 🔥 AQUI fica a lógica especial da Cantina
-        handleExit={({ player, scene }) => {
-          const exits = scene.exitTile;
-          if (!exits) return false;
-
-          const matchedExit = exits.find(
-            (exit: any) =>
-              player.gridX === exit.x &&
-              player.gridY === exit.y
-          );
-
-          if (matchedExit) {
-            if (matchedExit.requiredQuest) {
-              const hasQuest = quests.some(
-                (q) => q.id === matchedExit.requiredQuest
-              );
-
-              if (!hasQuest) {
-                setPopup(
-                  matchedExit.blockedMessage ||
-                    "Você não pode ir agora."
-                );
-                return true; // 🚨 bloqueia
-              }
-            }
-
-            navigate(matchedExit.route, {
-              state: { from: location.pathname },
-            });
-
-            return true;
-          }
-
-          return false;
-        }}
       />
 
       {/* ✅ popup continua fora do SceneBase */}
