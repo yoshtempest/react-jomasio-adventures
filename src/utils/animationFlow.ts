c7cfb84f2da18bb22f0ab6bb7f961538414cecc7
marcelo <marcelobenjaminrocha100@gmail.com>
2026-06-05 18:01:34 -0300

    fix: block duration is infinite

--- src/utils/animationFlow.ts
+++ src/utils/animationFlow.ts
@@ -16,19 +16,19 @@ type AnimationStep = {
 };
 
 export const animationFlow: Record<PlayerState, AnimationStep | null> = {
-    idle: null,
+  idle: null,
 
-    preAttack: { next: "attack", duration: 150 },
-    attack: { next: "idle", duration: 300 },
+  preAttack: { next: "attack", duration: 150 },
+  attack: { next: "idle", duration: 300 },
 
-    preWalk: { next: "walk", duration: 100 },
-    walk: null, // contínuo
+  preWalk: { next: "walk", duration: 100 },
+  walk: null, // contínuo
 
-    preJump: null,
-    jump: null,
+  preJump: null,
+  jump: null,
 
-    preSpecial: { next: "special", duration: 200 },
-    special: { next: "idle", duration: 500 },
+  preSpecial: { next: "special", duration: 200 },
+  special: { next: "idle", duration: 500 },
 
-    blocked: { next: "idle", duration: 200 },
+  blocked: null,
 };
