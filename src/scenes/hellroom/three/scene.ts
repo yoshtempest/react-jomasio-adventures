53e8daf9b496c72e942c2d87a0d807123efb8d4e
yoshtempest <marcelobenjaminrocha100@gmail.com>
2026-06-12 07:25:44 -0300

    new: hellRoom map

--- src/scenes/hellroom/three/scene.ts
+++ src/scenes/hellroom/three/scene.ts
@@ -1,16 +1,16 @@
-import { director } from "@/maps/director";
 import { MUSICS } from "@/scenes/shared/music";
 import type { SceneConfig } from "@/utils/types/maps/sceneConfig";
 import { getHellroomThreeInitialPosition } from "./position";
 import { hellroomThreeNpcs } from "./npcs";
 import { getHellroomThreeDialogue } from "./dialogue";
 import { hellroomThreeEvents } from "./events";
+import { hellRoomThree } from "@/maps/hellroom/three";
 
 export const threeScene: SceneConfig = {
     id: "three",
     initialPosition: getHellroomThreeInitialPosition,
     dialogueData: getHellroomThreeDialogue,
-    map: director,
+    map: hellRoomThree,
     events: hellroomThreeEvents,
     audio: { src: MUSICS.default },
     npcs: hellroomThreeNpcs,
--- src/scenes/hellroom/two/scene.ts
+++ src/scenes/hellroom/two/scene.ts
@@ -1,16 +1,16 @@
-import { director } from "@/maps/director";
 import { MUSICS } from "@/scenes/shared/music";
 import type { SceneConfig } from "@/utils/types/maps/sceneConfig";
 import { getHellroomTwoInitialPosition } from "./position";
 import { hellroomTwoNpcs } from "./npcs";
 import { getHellroomTwoDialogue } from "./dialogue";
 import { hellroomTwoEvents } from "./events";
+import { hellRoomTwo } from "@/maps/hellroom/two";
 
 export const twoScene: SceneConfig = {
     id: "two",
     initialPosition: getHellroomTwoInitialPosition,
     dialogueData: getHellroomTwoDialogue,
-    map: director,
+    map: hellRoomTwo,
     events: hellroomTwoEvents,
     audio: { src: MUSICS.default },
     npcs: hellroomTwoNpcs,
