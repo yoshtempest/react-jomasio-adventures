cb517a39be5e35e140e4553b7d038cdff97e497f
yoshtempest <marcelobenjaminrocha100@gmail.com>
2026-06-23 12:17:03 -0300

    new: hirose dialogues

---
+++ src/data/maps/skinShop/hirose.ts
@@ -0,0 +1,12 @@
+export const skinShopHiroseDialogue = [
+  {
+    src: "/assets/npcs/bruninho/right.svg",
+    name: "Bruno",
+    message: "Aah Nakamura...",
+  },
+  {
+    isPlayer: true,
+    name: "Protagonista",
+    message: "?",
+  },
+];
---
+++ src/data/maps/skinShop/nakamuraHirose.ts
@@ -0,0 +1,47 @@
+export const skinShopIfNakamuraDialogue = [
+  {
+    isPlayer: true,
+    name: "Protagonista",
+    message: "O que é isso que está acontecendo comigo? Estou mudando?",
+  },
+  {
+    src: "/assets/npcs/bruninho/right.svg",
+    name: "Bruninho",
+    message: "Nakamuraa~",
+  },
+  {
+    src: "/assets/player/marcelo/nakamura.svg",
+    name: "Marcelinho",
+    message: "Bom dia fofo",
+  },
+  {
+    src: "/assets/npcs/bruninho/right.svg",
+    name: "Bruninho",
+    message: "Bom dia gatinho",
+  },
+  {
+    src: "/assets/player/marcelo/nakamura.svg",
+    name: "Marcelinho",
+    message: "Senti saudades >-<",
+  },
+  {
+    src: "/assets/npcs/bruninho/right.svg",
+    name: "Bruninho",
+    message: "Me dá um abraçoo",
+  },
+  {
+    src: "/assets/player/marcelo/nakamura.svg",
+    name: "Marcelinho",
+    message: "Esperei tanto por esse dia!",
+  },
+  {
+    src: "/assets/npcs/bruninho/right.svg",
+    name: "Bruninho",
+    message: "Quer ir pro beco?",
+  },
+  {
+    src: "/assets/player/marcelo/nakamura.svg",
+    name: "Marcelinho",
+    message: "Sério?",
+  },
+];
