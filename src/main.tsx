f8cccb013b9b066c17bf08ba09d496e34c8281f8
marcelo <marcelobenjaminrocha100@gmail.com>
2026-07-02 11:34:01 -0300

    feat: separating index.css in 13 archives

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router";
-import "./index.css";
+import "./styles/index.css";
 
 import { AppProviders } from "@/contexts/AppProviders";
 import { AppRoutes } from "@/pages/AppRoutes";
