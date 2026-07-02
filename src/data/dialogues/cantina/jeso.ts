182c84e07881422645147cad605dafca29e8e42b
marcelo <marcelobenjaminrocha100@gmail.com>
2026-07-02 15:12:03 -0300

    feat: new Jeso npc and dialogue

---
+++ src/data/dialogues/cantina/jeso.ts
@@ -0,0 +1,17 @@
+export const cantinaJesoDialogue = [
+  {
+    src: "/assets/npcs/jeso/right.svg",
+    name: "Jeso",
+    message: "Eae, quer uma comida?",
+  },
+  {
+    isPlayer: true,
+    name: "Protagonista",
+    message: "Tô cagado de fome",
+  },
+  {
+    src: "/assets/npcs/jeso/right.svg",
+    name: "Jeso",
+    message: "Não era nesse sentido que eu tava falando, mas beleza, eu posso te dar uma comida.",
+  },
+];
