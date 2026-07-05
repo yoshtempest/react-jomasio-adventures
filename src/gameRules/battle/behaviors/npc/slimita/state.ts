3e0db51c6f06eb59425b3b815daa96ed13be299e
marcelo <marcelobenjaminrocha100@gmail.com>
2026-07-05 13:55:42 -0300

    feat: organizating npc sounds

--- src/gameRules/battle/behaviors/npc/slimita/state.ts
+++ src/gameRules/battle/behaviors/npc/slimita/state.ts
@@ -4,7 +4,7 @@ export const FAR_DISTANCE_X = 260;
 export const MELEE_RANGE = 50;
 
 export const boomAudio = new Audio(
-  asset("/assets/songs/soundEffects/npc/boom.mp3")
+  asset("/assets/songs/soundEffects/npc/slimita/boom.mp3")
 );
 
 boomAudio.volume = 0.7;
