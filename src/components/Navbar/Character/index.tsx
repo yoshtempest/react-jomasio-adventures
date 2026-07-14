d151443758689983a5ae6acd123267496b87b5b7
marcelo <marcelobenjaminrocha100@gmail.com>
2026-07-14 16:28:23 -0300

    fix: adjusting style of buttonSelected

--- src/components/Navbar/Character/index.tsx
+++ src/components/Navbar/Character/index.tsx
@@ -52,7 +52,7 @@ export function Character() {
               </p>
             </div>
             <button className={`${styles.button}
-            ${isSelected ? styles.selected : ""}`}>Selecionar</button>
+            ${isSelected ? styles.buttonSelected : ""}`}>Selecionar</button>
           </div>
         );
       })}
