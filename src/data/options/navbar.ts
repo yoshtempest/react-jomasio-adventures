342bc12a2ef206d55c87b37b4f5a4ec906bfceaa
marcelo <marcelobenjaminrocha100@gmail.com>
2026-07-15 14:23:23 -0300

    feat: adjusting visual order of navbar exibition to menus

--- src/data/options/navbar.ts
+++ src/data/options/navbar.ts
@@ -1,15 +1,15 @@
 import type { NavbarOption } from "@/utils/types/player/navbar";
 
 export const NAVBAR_OPTIONS: NavbarOption[] = [
-  { icon: "/assets/navbar/configs.svg", label: "Configurações", screen: "config" },
+  { icon: "/assets/navbar/player.svg", label: "Jogador", screen: "player" },
   { icon: "/assets/navbar/character.svg", label: "Personagem", screen: "character" },
   { icon: "/assets/navbar/status.svg", label: "Status", screen: "status" },
-  { icon: "/assets/navbar/quests.svg", label: "Missões", screen: "missions" },
   { icon: "/assets/navbar/equipments.svg", label: "Equipamentos", screen: "equipment" },
   { icon: "/assets/navbar/backpack.svg", label: "Mochila", screen: "inventory" },
-  { icon: "/assets/navbar/titles.svg", label: "Títulos", screen: "titles" },
-  { icon: "/assets/navbar/professions.svg", label: "Profissões", screen: "professions" },
-  { icon: "/assets/navbar/player.svg", label: "Jogador", screen: "player" },
+  { icon: "/assets/navbar/quests.svg", label: "Missões", screen: "missions" },
   { icon: "/assets/navbar/deliciaDex.svg", label: "DelíciaDex", screen: "bestiary" },
+  { icon: "/assets/navbar/professions.svg", label: "Profissões", screen: "professions" },
+  { icon: "/assets/navbar/titles.svg", label: "Títulos", screen: "titles" },
   { icon: "/assets/navbar/saves.svg", label: "Saves", screen: "saves" },
+  { icon: "/assets/navbar/configs.svg", label: "Configurações", screen: "config" },
 ];
