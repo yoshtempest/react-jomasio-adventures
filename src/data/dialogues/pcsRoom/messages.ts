b9acaf88fa29b52dd3653a76b9047b1fb7eb1502
yoshtempest <marcelobenjaminrocha100@gmail.com>
2026-08-04 09:19:54 -0300

    feat: adjusting interactions of pcroom

--- src/data/dialogues/pcsRoom/messages.ts
+++ src/data/dialogues/pcsRoom/messages.ts
@@ -1,35 +1,35 @@
 export const pcsRoomMessages: Record<string, string> = {
-  "3,3": "Macaco girando, eu gosto disso.",
-  "4,3": "As irmãs planetárias podem ser vistas a quilomêtros de distância.",
-  "5,3":
+  "4,2": "Macaco girando, eu gosto disso.",
+  "5,2": "As irmãs planetárias podem ser vistas a quilomêtros de distância.",
+  "6,2":
     "A sinceridade é a fórmula de solução para muitos problemas desnecessários.",
-  "6,3": "11? O que isso significa?",
-  "9,3": "Ao ficar com fome por muito tempo os famintos começam a ouvir vozes",
-  "10,3": "As vozes não param, é preciso remédios",
-  "11,3":
+  "7,2": "11? O que isso significa?",
+  "11,2": "Ao ficar com fome por muito tempo os famintos começam a ouvir vozes",
+  "12,2": "As vozes não param, é preciso remédios",
+  "13,2":
     "Doutor tampinha afirma que Vodka contêm pouquissimos nutrientes, portanto deve ser ingerida em grande quantidades",
-  "12,3":
+  "14,2":
     "O jorjão pode ser estranho e assustador mas pelo menos é livre dos perigos que tem do lado de fora",
-  "13,3": "Damn Reincarnation cap novo? MENTIRA!",
-  "14,3":
+  "15,2": "Damn Reincarnation cap novo? MENTIRA!",
+  "16,2":
     "Diário de Reincardion cap 3... Aprendi a usar a internet, mas aqui não tem internet, triste, não?",
 
-  "4,5":
+  "5,4":
     "Um homem louco começou uma conversa aqui, ele alega ser do culto do bode",
-  "5,5":
+  "6,4":
     "Uma vez, a muito tempo atrás, foi descoberto a forma de deixar um trouxa curioso...",
-  "6,5":
+  "7,4":
     "Você sabia que no principio, qualquer inimigo tinha uma vida proporcional a pelo menos 5 vidas do jogador?",
-  "7,5":
+  "11,4":
     "Se você apertar a tecla g ou clicar no retângulo redondo, você pode ver o tutorial",
-  "8,5": "Basta ir nas configs do jogo",
-  "9,5": "Musquito orquestra.",
-  "10,5":
+  "12,4": "Basta ir nas configs do jogo",
+  "13,4": "Musquito orquestra.",
+  "14,4":
     "O desenvolvedor perdeu... Perdeu horas e horas de sua vida criando esse jogo, tenha compreensão com qualquer bug",
-  "11,5": "Foram 40 horas de trabalho em 4 dias, ",
-  "12,5":
+  "15,4":
+    "Foram 40 horas de trabalho em 4 dias, ",
+  "16,4":
     "O jogo só é salvo no refeitório, então você precisa jogar pelo menos até chegar lá antes de fechar o jogo",
-  "13,5": "Jailson Mendes, ele aguentou muita coisa por trás...",
-  "14,5":
+  "17,4":
     "Ataque normal, especial, awakening, classes, leveis, posição, alcance, bloqueio, pulo... Há muitas variáveis durante a batalha.",
 };
