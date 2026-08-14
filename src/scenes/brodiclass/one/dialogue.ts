57cb8189170d4518bfef2be26244312594960f97
yoshtempest <marcelobenjaminrocha100@gmail.com>
2026-08-14 08:24:26 -0300

    fix: dialogue by quest

--- src/scenes/brodiclass/one/dialogue.ts
+++ src/scenes/brodiclass/one/dialogue.ts
@@ -1,5 +1,12 @@
 import { brodiClassDialogue } from "@/data/dialogues/brodiclass/one";
+import { brodiClassTwoDialogue } from "@/data/dialogues/brodiclass/two";
+import { hasQuest } from "@/scenes/shared/helpers";
 
-export const getBrodiclassOneDialogue = () => {
+export const getBrodiclassOneDialogue = ({quests}: {
+  quests: { id: string }[];
+}) => {
+  if (hasQuest(quests, "save_ematron")) {
+    return brodiClassTwoDialogue;
+  }
   return brodiClassDialogue;
 };
