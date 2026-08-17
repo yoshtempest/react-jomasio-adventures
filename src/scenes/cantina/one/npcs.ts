633214667a358db409193eede3acca6669f4d204
yoshtempest <marcelobenjaminrocha100@gmail.com>
2026-08-17 12:37:09 -0300

    fix: adjusting jhowsimarNpcSprite in cantinaOne

--- src/scenes/cantina/one/npcs.ts
+++ src/scenes/cantina/one/npcs.ts
@@ -3,6 +3,12 @@ import { npcPath } from "@/utils/paths";
 import { createNpc } from "@/scenes/shared/factories";
 
 export const cantinaOneNpcs = [
-  createNpc(npcPath("/jhowsimar/default.svg"), 11, 3),
+  createNpc(
+    (ctx) => ctx.quests.some(q => q.id === "director_escape" && q.completed)
+      ? npcPath("/jhowsimar/sleeping.svg")
+      : npcPath("/jhowsimar/default.svg"),
+    11,
+    3
+  ),
   createNpc(npcPath("/brothers/default.svg"), 19, 3),
 ];
